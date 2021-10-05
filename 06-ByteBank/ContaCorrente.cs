d5acd4ea0f1a89071863524daa2ec4b3fbdd4146
Diego <dsctele@gmail.com>
2021-10-04 22:14:58 -0300

    20211004_noite

--- 06-ByteBank/ContaCorrente.cs
+++ 06-ByteBank/ContaCorrente.cs
@@ -5,43 +5,64 @@ namespace _06_ByteBank
 
     public class ContaCorrente
     {
-        public Cliente titular;
-        public int agencia;
-        public int numero;
-        public double saldo = 100;
-
-        public bool Sacar(double valor)
+        public Cliente Titular
         {
-            if (this.saldo < valor)
-            {
-                return false;
-            }
-            else
-            {
-                this.saldo -= valor;
-                return true;
-            }
+            get;
+            set;
         }
-
-        public void Depositar(double valor)
-        {
-            this.saldo += valor;
-        }
-        public bool Transferir(double valor, ContaCorrente contaDestino)
-        {
-            if (this.saldo < valor)
-            {
-                return false;
-            }
-            else
-            {
-                this.saldo -= valor;
-                contaDestino.Depositar(valor);
-                return true;
-            }
-        }
-
-
-
     }
+
+    public int agencia;
+    public int numero;
+    private double _saldo = 100;
+
+    public double Saldo
+    {
+        get
+        {
+            return _saldo;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                return;
+            }
+            _saldo = value;
+        }
+    }
+    public bool Sacar(double valor)
+    {
+        if (_saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            _saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        _saldo += valor;
+    }
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (_saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+
+
+
+}
 }
--- 06-ByteBank/Program.cs
+++ 06-ByteBank/Program.cs
@@ -6,9 +6,20 @@ namespace _06_ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente = ContaCorrente = new ContaCorrente();
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
 
-            ContaCorrente.saldo = -
+            cliente.nome = "Guilherme";
+            cliente.cpf = "059.319.539-65";
+            cliente.profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+
+            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Saldo);
+
         }
     }
 }
